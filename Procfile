cf822b4b15edd13293696403e0b61f8303d9b870
lokeshmunagala <78707972+lokeshmunagala@users.noreply.github.com>
2022-02-23 09:21:03 -0800

    updates

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn <project-name>.wsgi
+web: gunicorn swe1app.wsgi
--- swe1app/settings.py
+++ swe1app/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-import django_heroku
+
 
 
 # Application definition
